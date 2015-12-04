fbdb3f2d396b4a2b8195c2cc02106376996aaad6
MouradB <mourad_bzd@hotmail.fr>
2015-12-05 00:17:58 +0100

    Configure Git and First commit of projetSystemeI4

---
+++ application/configs/fermer_session.php
@@ -0,0 +1,5 @@
+<?php 
+	// Destruction de la session.
+	session_destroy();
+?>
+Votre session a bien été détruite merci de cliquer sur ce lien pour revenir à l'<a href="index.php">accueil</a>
---
+++ application/configs/livres.php
@@ -0,0 +1,26 @@
+<?php 
+	// Liste des livres commandés par l'utilisateur.
+	$requete = "SELECT * FROM tp3_livre INNER JOIN tp3_commande ON (id_livre = livre_id) WHERE utilisateur_id = ".$bd->quote($_SESSION['identifiant_utilisateur']);
+	$resultat = $bd->query($requete);
+	$livres = $resultat->fetchAll(PDO::FETCH_ASSOC);
+	if($livres > 0) {
+		echo '<ul>';
+		foreach($livres as $livre) {
+			echo '<li>';
+				echo $livre['titre'].' - ';
+				echo $livre['auteurs'].' - ';
+				echo $livre['nb_pages'].' - ';
+				if(file_exists('livres/'.$livre['fichier'])) {
+					echo '<a href="livres/'.$livre['fichier'].'">Télécharger</a>';
+				}
+				else {
+					echo 'Nous contacter car le livre a été supprimé de notre serveur.';
+				}
+			echo '</li>';
+		}
+		echo '</ul>';
+	}
+	else {
+		echo "Vous n'avez encore acheté aucun livre";
+	}
+?>
