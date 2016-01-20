4f15b22ad18bcd993a246540f8387c098d00a61d
mourad <mourad_bzd@hotmail.fr>
2016-01-20 09:02:05 +0100

    Changement de toute l'architecture - Remove ZendFramework - utilisation MVC (inspiré Symfony)

--- application/views/scripts/index/pages/fermer_session.php
+++
@@ -1,5 +0,0 @@
-<?php 
-	// Destruction de la session.
-	session_destroy();
-?>
-Votre session a bien été détruite merci de cliquer sur ce lien pour revenir à l'<a href="index.php">accueil</a>
--- public/js/jquery-1.0/build/js/writeFile.js
+++
@@ -1,7 +0,0 @@
-importPackage(java.io);
-
-function writeFile( file, stream ) {
-	var buffer = new PrintWriter( new FileWriter( file ) );
-	buffer.print( stream );
-	buffer.close();
-}
