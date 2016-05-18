aed1b322416a4b0454d1e15d7b6a30e6eaee62d6
mourad1b <mourad_bzd@hotmail.fr>
2016-05-19 01:55:32 +0200

    Ajout Controller d'envoi de Mail avec PHPMailer

---
+++ Controller/MailSenderController.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * MailSenderController.php
+ */
+
+namespace nsNewsletter\Controller;
+
+class MailSenderController
+{
+    public function send($mail_destinataires, $subject, $message, $groupe = null, $params = null)
+    {
+        require '../Lib/PHPMailer/PHPMailerAutoload.php';
+        $mail = new \PHPMailer();
+
+        //$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+        $mail->isSMTP();                                      // Set mailer to use SMTP
+        $mail->Host = 'smtp.live.com';  // Specify main and backup SMTP servers
+        $mail->SMTPAuth = true;                               // Enable SMTP authentication
+        $mail->Username = 'news.projet@hotmail.com';                 // SMTP username
+        $mail->Password = 'Epsi2015';                           // SMTP password
+        $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+        $mail->Port = 587; // 587   465                             // TCP port to connect to
+
+        $mail->setFrom('news.projet@hotmail.com', 'Projet Service Newsletter');
+        //$mail->addAddress('news.projet@hotmail.com', 'Mourad');     // Add a recipient
+        $mail->addAddress($mail_destinataires);               // Name is optional
+        $mail->addReplyTo('news.projet@hotmail.com', 'Information - Service Newsletter');
+
+        $mail->isHTML(true);                                  // Set email format to HTML
+
+        $mail->Subject = $subject; // 'Here is the subject';
+        $mail->Body    = $message; //'This is the HTML message body <b>in bold!</b>';
+        //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+        if(!$mail->send()) {
+            echo 'Message could not be sent.';
+            echo 'Mailer Error: ' . $mail->ErrorInfo;
+        } else {
+            echo 'Message has been sent';
+        }
+    }
+
+}
+
--- View/Newsletter/addNewsletterForm2.php
+++ View/Newsletter/addNewsletterForm2.php
@@ -140,6 +140,7 @@
          '//fast.fonts.net/cssapi/e6dc9b99-64fe-4292-ad98-6974f93cd2a2.css',
          '//www.tinymce.com/css/codepen.min.css'
          ]*/
+
     });
 
 
